c7b6cf78336ea401c2bf1a5368e8f583033b27ad
Lemi Dinku <112744867+Lemidinku@users.noreply.github.com>
2026-08-19 15:46:47 +0300

    Time: 5171 ms (12.18%), Space: 19.7 MB (8.16%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,23 +1,24 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         m , n = len(board), len(board[0])
-        size = len(word)
         dir = [[0,1], [0,-1], [1, 0], [-1,0]]
 
         def is_inbound(r,c):
             return 0<= r < m and 0<= c < n
 
-        def search(ind, r, c, visited):
-            if ind+1==size:
+        def search(word, r, c, visited):
+            if board[r][c] != word[0]:
+                return False
+            if len(word)==1:
                 return True
+
             visited.add((r,c))
             found = False
 
             for x,y in dir:
                 new_r, new_c = r+x, c+y
                 if is_inbound(new_r, new_c) and (new_r, new_c) not in visited:
-                    if board[new_r][new_c] == word[ind+1]:
-                        found |= search(ind+1, new_r, new_c, visited)
+                    found |= search(word[1:], new_r, new_c, visited)
             visited.remove((r,c))
 
 
@@ -26,6 +27,6 @@ class Solution:
         
         for r in range(m):
             for c in range(n):
-                if word[0] == board[r][c] and search(0, r,c, set()):
+                if search(word, r,c, set()):
                     return True
         return False
